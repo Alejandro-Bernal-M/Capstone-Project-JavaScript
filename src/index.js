72c34e0443ffb42443fe8467c06b7b7c57cdb2f8
alejandro <alejandrober96@gmail.com>
2023-03-14 17:06:35 -0500

    linters and configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './styles.css';
 import cardsConstructor from './modules/cardsConstructor.js';
+import getLikes from './modules/getLikes.js';
+import giveLikes from './modules/giveLikes.js';
+import likeCounter from './modules/likeCounter.js';
 
 cardsConstructor();
+giveLikes();
+window.onload = () => {
+  getLikes();
+  setInterval(likeCounter, 1000);
+};
