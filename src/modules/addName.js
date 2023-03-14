c1bdcbbc30d8f45873dfd869baf6cb1f4e9eab96
alejandro <alejandrober96@gmail.com>
2023-03-14 12:47:38 -0500

    Layout using API and JavaScript

---
+++ src/modules/addName.js
@@ -0,0 +1,3 @@
+export default async (obj, elem) => {
+ elem.innerHTML = `${obj.nome}`
+}
---
+++ src/modules/carImg.js
@@ -0,0 +1,29 @@
+import acura from '../CARSMODELS/acura.jpg';
+import agrale from '../CARSMODELS/agrale.jpg';
+import alfa from '../CARSMODELS/alfaromio.jpg';
+import AMGEN from '../CARSMODELS/AMGEN.jpg';
+import asia from '../CARSMODELS/asiamotors.jpg';
+import audi from '../CARSMODELS/audi.jpg';
+
+const cars = [
+  {
+    src: acura,
+  },
+  {
+    src: agrale,
+  },
+  {
+    src: alfa,
+  },
+  {
+    src: AMGEN,
+  },
+  {
+    src: asia,
+  },
+  {
+    src: audi,
+  },
+];
+
+export { cars };
