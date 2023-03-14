c052128719d0526173d3c102def9029172321e68
maher <maherramadan2012@hotmail.com>
2023-03-14 16:47:21 -0600

    Create the pop up.js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ import './styles.css';
 import cardsConstructor from './modules/cardsConstructor.js';
 
 cardsConstructor();
+
+
+if (button) {
+    const selectedShow = shows.filter(
+      (it) => it.id.toString() === button.id.toString(),
+    )[0];
+    const {
+      id, image, Model,price,color,
+    } = selectedShow;
+    showContainer.style.display = 'none';
+    popupContainer.style.display = 'block';
+    Popup(id, image, Model, price, color);
+  }
+
+if (closeButton) {
+    popupContainer.style.display = 'none';
+    showContainer.style.display = 'grid';
+    cardsConstructor();
+  }
+
---
+++ src/modules/popup.js
@@ -0,0 +1,25 @@
+const popupContainer = document.querySelector('.popup-display');
+
+const Popup = async (
+    id,
+    image,
+    Model,
+    price,
+    color,   
+) => {
+    popupContainer.innerHTML = '';
+
+    popupContainer.innerHTML += `
+    <div class = "best-cars-${id} cars-img-section">
+        <img src=${image.original} alt="" class="cars-img" />
+        <div class="close-icon">X</div>
+      </div>
+
+      <h2 class = "cars-popup-title">${Model}</h2>
+      <div class="movie-features">
+      <p>price: ${price}</p>
+      <p>color: ${color}</p>
+      </div>
+
+      `;
+};
