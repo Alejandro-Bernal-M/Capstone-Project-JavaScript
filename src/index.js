e5ac503f599290e2c1270fa5f292a2b73ba10093
alejandro <alejandrober96@gmail.com>
2023-03-15 11:13:14 -0500

    popup

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import './styles.css';
 import cardsConstructor from './modules/cardsConstructor.js';
+import arrayOfCars from './modules/arrayWithCars.js';
 
 cardsConstructor();
 
 
-if (button) {
-    const selectedShow = shows.filter(
-      (it) => it.id.toString() === button.id.toString(),
-    )[0];
-    const {
-      id, image, Model,price,color,
-    } = selectedShow;
-    showContainer.style.display = 'none';
-    popupContainer.style.display = 'block';
-    Popup(id, image, Model, price, color);
-  }
+// if (button) {
+//     const selectedShow = shows.filter(
+//       (it) => it.id.toString() === button.id.toString(),
+//     )[0];
+//     const {
+//       id, image, Model,price,color,
+//     } = selectedShow;
+//     showContainer.style.display = 'none';
+//     popupContainer.style.display = 'block';
+//     Popup(id, image, Model, price, color);
+//   }
 
-if (closeButton) {
-    popupContainer.style.display = 'none';
-    showContainer.style.display = 'grid';
-    cardsConstructor();
-  }
+// if (closeButton) {
+//     popupContainer.style.display = 'none';
+//     showContainer.style.display = 'grid';
+//     cardsConstructor();
+//   }
 
