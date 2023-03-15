b484e2b13021291a018539633631e97867b6efc8
alejandro <alejandrober96@gmail.com>
2023-03-15 12:41:25 -0500

    counters

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import cardsConstructor from './modules/cardsConstructor.js';
 import getLikes from './modules/getLikes.js';
 import giveLikes from './modules/giveLikes.js';
 import likeCounter from './modules/likeCounter.js';
+import counterForAllLikes from './modules/counterForAllLikes.js';
+
+const counterLikesHolder = document.querySelector('.likesCounter')
+
+counterForAllLikes();
 
 cardsConstructor();
 giveLikes();
+
+setInterval(async () =>{
+ counterLikesHolder.textContent = await counterForAllLikes(); }, 1000);
+
 window.onload = () => {
   getLikes();
   setInterval(likeCounter, 1000);
