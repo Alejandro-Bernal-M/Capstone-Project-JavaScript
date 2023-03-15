d35c187496e77016d02542b0466cef52bb48d62f
alejandro <alejandrober96@gmail.com>
2023-03-15 16:46:19 -0500

    Test for counter function done

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,9 +1,22 @@
 import counterForAllCards from '../modules/counterForAllCards.js';
 
 describe('Test for counter all the items', () => {
-  test('should be 1', () =>{
+  test('should be 1', () => {
     document.body.innerHTML = `
     <div class="card"></div>
     `;
+    expect(counterForAllCards()).toEqual(1);
+  })
+
+  test('should be 6', () => {
+    document.body.innerHTML = `
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    `;
+    expect(counterForAllCards()).toEqual(6);
   })
 })
