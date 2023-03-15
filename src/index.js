11fbdd3f9a8ef83448ac7b893fa3d93bdead0ba5
alejandro <alejandrober96@gmail.com>
2023-03-15 14:25:28 -0500

    Functionality for count all the cards and likes finished

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import getLikes from './modules/getLikes.js';
 import giveLikes from './modules/giveLikes.js';
 import likeCounter from './modules/likeCounter.js';
 import counterForAllLikes from './modules/counterForAllLikes.js';
+import counterForAllCards from './modules/counterForAllCards.js';
 
-const counterLikesHolder = document.querySelector('.likesCounter')
+const counterCarsHolder = document.querySelector('.carsCounter');
+const counterLikesHolder = document.querySelector('.likesCounter');
 
 counterForAllLikes();
-
 cardsConstructor();
+counterCarsHolder.textContent = counterForAllCards();
 giveLikes();
 
-setInterval(async () =>{
- counterLikesHolder.textContent = await counterForAllLikes(); }, 1000);
+setInterval(async () => {
+  counterLikesHolder.textContent = await counterForAllLikes();
+}, 1000);
 
 window.onload = () => {
   getLikes();
--- src/modules/counterForAllLikes.js
+++ src/modules/counterForAllLikes.js
@@ -9,4 +9,4 @@ export default async () => {
     }
   });
   return counter;
-}
+};
