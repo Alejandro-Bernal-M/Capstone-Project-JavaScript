07cb848189397af384596d5541b72f2b35c0fa15
maher <maherramadan2012@hotmail.com>
2023-03-15 15:23:51 -0600

    Update comment .js file

--- src/modules/getcomments.js
+++ src/modules/getcomments.js
@@ -1,5 +1,5 @@
 export default async () => {
-  const comments = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hKLSytbuJWenzwwIdsnm/comments?item_id=1/').then((response) => response.json());
+  const comments = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/hKLSytbuJWenzwwIdsnm/comments?item_id=example/').then((response) => response.json());
   const data = await comments;
   console.log(data);
   return data;
